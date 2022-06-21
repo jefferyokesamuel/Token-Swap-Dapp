e15f24f13a70853111e50148cddaec9429419b7f
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-06-21 18:42:41 +0100

    Writing functionality

--- src/contracts/DBank.sol
+++ src/contracts/DBank.sol
@@ -9,8 +9,22 @@ contract DBank {
     Tether public tether;
     Reward public reward;
 
+    address[] public stakers;
+
+    mapping (address => uint256) public stakingBalance;
+    mapping (address => bool) public hasStaked;
+    mapping (address => bool) public isStaked;
+
     constructor (Reward _reward, Tether _tether) public {
         reward = _reward;
         tether = _tether;
+    }
+
+    function depositTokens (uint _amount) public {
+        // Transfer tokens to contrack address for staking
+        tether.transferFrom(msg.sender, address(this), _amount);
+        //Update staking balance
+
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
     }
 }
