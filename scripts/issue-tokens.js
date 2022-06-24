29d0b05fdccd9f1ce0bfc8a6bcb850c18d6a2b8b
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-06-24 17:48:39 +0100

    changes

--- scripts/issue-tokens.js
+++ scripts/issue-tokens.js
@@ -1,7 +1,7 @@
-const dbank = artifacts.require('DBank')
+const DBank = artifacts.require('DBank')
 
 module.exports = async function issueRewards(callback) {
-    let dbank = await dbank.deployed()
+    let dbank = await DBank.deployed()
     await dbank.issueTokens()
     console.log('The Tokens have been issued')
     callback()
