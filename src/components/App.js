137e989b1e20ccc206c2b64fc2ede0ee2dc7b237
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-06-24 18:33:17 +0100

    making the ui

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import './App.css'
+class App extends Component {
+    // React code goes here
+    render () {
+        return (
+            <div className='text-center'>
+                <h>Hello World</h>
+            </div>
+        )
+    }
+}
+
+export default App;
