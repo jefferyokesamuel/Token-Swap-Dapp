eb909d567edb8fb2f1a41f2065bc8f6d272f378a
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-06-25 10:06:58 +0100

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,18 @@ import './App.css'
 import Navbar from './Navbar.js'
 
 class App extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            account: '0x0'
+        }
+    }
     // React code goes here 
     render () {
         return (
             <div>
-                <Navbar />
+                <Navbar account={this.state.account}/>
                 <div className='text-center'>
                     <h>Hello World</h>
                  </div>
