21f83a8fa0a897d42394872a3a3a9f8de32ae94d
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-06-25 06:03:54 +0100

    Creating the UI with react

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react';
+import bank from '../bank.png';
 
 class Navbar extends Component {
 
     render () {
         return (
-            <nav className='navbar navbar-dark fixed-top shadow p-0' style={{backgroundcolor:'black', height: '50px'}}>
-                <a style={{color:'white'}}>Token Swap</a>
-                <ul>
-                    <li>
-                        <small style={{color:'white'}}> Account Number
+            <nav className='navbar navbar-dark fixed-top shadow p-0' style={{backgroundColor:'black', height: '50px'}}>
+                <a className='navbar-brand col-sm-3 col-md-2 mr-0'
+                style={{color:'white'}}>
+                <img src={bank} width ='50' height='30' className='d-inline-block align-top' alt='Bank icon'/>
+                &nbsp; Token Swap (Decentralised Banking)
+                </a>
+                <ul className='navbar-nav px-3'>
+                    <li className='text-nowrap d-none nav-item d-sm-none d-sm-block'>
+                        <small style={{color:'white'}}> ACCOUNT NUMBER:
 
                         </small>
                     </li>
