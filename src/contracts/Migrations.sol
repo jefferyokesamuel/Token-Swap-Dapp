b4d14e3711d9efabf6174330f33908311cf8b3f9
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-06-28 23:20:24 +0100

    changes

--- src/contracts/Migrations.sol
+++ src/contracts/Migrations.sol
@@ -4,10 +4,11 @@ contract Migrations {
     address public owner;
     uint public last_completed_migration;
 
+
     constructor() public {
         owner = msg.sender;
     }
-    
+
     modifier restricted() {
         if (msg.sender == owner) _;   
     }
