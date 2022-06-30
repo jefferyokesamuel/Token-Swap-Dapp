612356afb562f5cee6833ba703faa2c72a64cccd
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-06-30 04:04:06 +0100

    creating the UI

--- src/components/Main.js
+++ src/components/Main.js
@@ -26,20 +26,25 @@ class Main extends Component {
                             <span className='float-right' style={{marginRight: '8px'}}>
                                 Balance:
                             </span>
-                            <div className='input-gropu mb-4'>
+                            <div className='input-group mb-4'>
                                 <input 
                                 type="text"
                                 placeholder='0'
                                 required />
                                 <div className='input-grouped-open'>
                                     <div className='input-group-text'>
-                                        <img alt='tether' src={tether} />
-                                        &nbps;&nbps;&nbps;USDT
+                                        <img src={tether} alt='tether' height='32' />
+                                        &nbsp;&nbsp;&nbsp;USDT
                                     </div>
                                 </div>
                             </div>
+                            <button type='submit' className='btn btn-primary btn-lg btn-block'>DEPOSIT</button>
                         </div>
                     </form>
+                    <button type='submit' className='btn btn-primary btn-lg btn-block'>WITHDRAW</button>
+                    <div className='card-body text-center' style={{color: 'blue'}}>
+                        AIRDROP
+                    </div>
                 </div>
             </div>
         )
