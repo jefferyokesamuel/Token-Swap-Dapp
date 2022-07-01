57ba8fbc41a47af9be07d1a25cbfe819ee9cb43f
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-07-01 13:24:13 +0100

    Unstaking Functionalities

--- src/components/Main.js
+++ src/components/Main.js
@@ -20,16 +20,25 @@ class Main extends Component {
                     </tbody>
                 </table>
                 <div className='card mb-2' style={{opacity: '.9'}}>
-                    <form className='mb-3'>
+                    <form 
+                    onSubmit={(event) => {
+                        event.preventDefault()
+                        let amount
+                        amount = this.input.value.toString()
+                        amount = window.web3.utils.toWei(amount, 'Ether')
+                        this.props.stakeTokens(amount)
+                    }}
+                    className='mb-3'>
                         <div style={{borderSpacing: '0 1em'}}>
                             <label className='float-left' style={{marginLeft: '15px'}}><b>State Token</b></label>
                             <span className='float-right' style={{marginRight: '8px'}}>
                                 Balance: {window.web3.utils.fromWei(this.props.tetherBalance, 'Ether')}
                             </span>
                             <div className='input-group mb-4'>
-                                <input 
+                                <input
+                                ref={(input) =>{this.input = input}}  
                                 type="text"
-                                placeholder='0'
+                                placeholder='0' 
                                 required />
                                 <div className='input-group-open'>
                                     <div className='input-group-text'>
@@ -41,7 +50,12 @@ class Main extends Component {
                             <button type='submit' className='btn btn-primary btn-lg btn-block'>DEPOSIT</button>
                         </div>
                     </form>
-                    <button type='submit' className='btn btn-primary btn-lg btn-block'>WITHDRAW</button>
+                    <button
+                     onSubmit={(event) => { 
+                        event.preventDefault()
+                        this.props.unstakeTokens
+                    }}
+                    type='submit' className='btn btn-primary btn-lg btn-block'>WITHDRAW</button>
                     <div className='card-body text-center' style={{color: 'blue'}}>
                         AIRDROP
                     </div>
